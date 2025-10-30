6df3434b9c9d78caa3c244698d6a336e49a6094b
zoedays <zoedaych@gmail.com>
2025-10-29 22:18:30 -0500

    Update session timeout in ControlLogin servlet
    Increased session timeout from 30 to 90 minutes.

--- src/java/controler/ControlLogin.java
+++ src/java/controler/ControlLogin.java
@@ -23,7 +23,7 @@ public class ControlLogin extends HttpServlet {
 
         if (usuAut != null){
             HttpSession session = request.getSession();
-            session.setMaxInactiveInterval(30);
+            session.setMaxInactiveInterval(90);
             session.setAttribute("usuAut", usuAut); 
             response.sendRedirect("index.jsp");
             System.out.println("Sesión iniciada correctamente");
@@ -50,3 +50,4 @@ public class ControlLogin extends HttpServlet {
     }// </editor-fold>
 
 }
+
